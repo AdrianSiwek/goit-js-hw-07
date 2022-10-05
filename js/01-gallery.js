b90ddf0273f3f92696922d8b3a28e2f8aa6fc041
AdrianSiwek <105585774+AdrianSiwek@users.noreply.github.com>
2022-10-05 23:56:49 +0200

    update

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,26 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallery = document.querySelector("div.gallery");
+const instance = basicLightbox.create(`<img src="" />`);
+
+const newImg = galleryItems
+    .map(
+        (image) => `<div class = "gallery_items">
+    <a class = "gallery__links" href="${image.original}">
+    <image
+    class="gallery_img"
+    src="${image.preview}"
+    data-source="${image.original}"
+    alt="${image.description}"
+    />
+    </a>
+    </div>`
+    )
+    .join = ("");
+
+gallery.insertAdjacentHTML("beforeend", newImg);
+gallery.addEventListener("click", onOpenModal);
+
+const url = instance.element().querySelector("img");
 
 console.log(galleryItems);
