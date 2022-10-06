54b4590df95210aaa96243ab05edd4d8311024fc
AdrianSiwek <105585774+AdrianSiwek@users.noreply.github.com>
2022-10-06 22:57:16 +0200

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,27 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallery = document.querySelector("ul.gallery");
+
+
+const newImg = galleryItems
+    .map(
+        (image) =>
+            `<a class="gallery__link" href="${image.original}">
+    <img
+      class="gallery__image"
+      src="${image.preview}"
+      alt="${image.description}"
+    />
+  </a>`
+    )
+    .join("");
+
+gallery.insertAdjacentHTML("beforeend", newImg);
+
+const light = new SimpleLightbox('.gallery a', {
+    captionDelay: 250,
+    captionsData: "alt",
+    captionPosition: "top",
+});
 
 console.log(galleryItems);
